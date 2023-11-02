b2cb6f77d8426beb40de879b3bfe8d2793b8be53
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-11-01 18:27:43 -0600

    Añadiendo y configurando TailwindCSS al proyecto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title> DAW @yield('title')</title>
+  @vite('resources/css/app.css')
 </head>
 
 <body>
+  <h1 class="text-4xl font-bold">@yield('title')</h1>
 </body>
 
 </html>
