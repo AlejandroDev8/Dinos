e3bccef66c1c1e215cf117a3cd0a2b58a153326d
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-11-05 17:56:25 -0600

    Añadiendo la migracion para username

---
+++ database/migrations/2023_11_05_233227_add_username_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+
+  // El método de up se ejecuta cuando se ejecuta la migración
+  public function up(): void
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->string('username');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+
+  // El método de down se ejecuta cuando se revierte la migración
+  public function down(): void
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->dropColumn('username');
+    });
+  }
+};
