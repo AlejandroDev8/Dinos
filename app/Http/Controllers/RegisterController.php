f60a815160032886cfd6e65e0d2c6790327f400e
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-11-10 17:15:00 -0600

    Iniciando la creación de registros con Eloquent ORM

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -23,6 +24,15 @@ class RegisterController extends Controller
       'username' => 'required|max:20|unique:users|min:3|max:15',
       'email' => 'required|email|unique:users|max:50',
       'password' => 'required|min:6|confirmed'
+    ]);
+
+    // Creamos el registro del usuario
+
+    User::create([ //El "create", es el equivalente a "insert into" de SQL
+      'name' => $request->name,
+      'username' => $request->username,
+      'email' => $request->email,
+      'password' => bcrypt($request->password)
     ]);
   }
 }
