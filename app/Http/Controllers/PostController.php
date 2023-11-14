eff9620d3d8a2b83c2dc7fd21995691f8fd02e39
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-11-13 22:10:02 -0600

    Creando el muro

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
+  public function __construct()
+  {
+    $this->middleware(['auth']);
+  }
+
+
   public function index()
   {
-    dd(auth()->user());
+    return view('dashboard');
   }
 }
