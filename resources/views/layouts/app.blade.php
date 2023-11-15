b7609d937ff33ddec9721bb59ac32bbf0e019f67
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-11-14 18:46:00 -0600

    Revisando si el usuario está autenticado o no

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,24 @@
   <header class="p-5 border-b bg-white shadow">
     <div class="container mx-auto flex justify-between items-center">
       <a href="/" class="text-3xl font-black uppercase">dinos</a>
+      {{-- Para saber que un usuario está autenticado --}}
+      @auth
       <nav class="flex gap-3 items-center">
-        <a class="font-bold uppercase text-gray-500 text-lg hover:text-gray-900" href="{{route('login')}}">Login</a>
+        <a class="font-bold text-gray-500 text-lg hover:text-gray-900" href="{{route('login')}}">
+          Hola <span class="font-normal">{{auth()->user()->username}}</span>
+        </a>
         {{-- Con el "route" traemos el nombre de la ruta nombrada --}}
-        <a class="font-bold uppercase text-gray-500 text-lg hover:text-gray-900" href="{{route('signup')}}">SignUp</a>
+        <a class="font-bold uppercase text-gray-500 text-lg hover:text-gray-900" href="{{route('signup')}}">cerrar sesión</a>
       </nav>
+      @endauth
+      {{-- Para saber que un usuario no está autenticado --}}
+      @guest  
+        <nav class="flex gap-3 items-center">
+          <a class="font-bold uppercase text-gray-500 text-lg hover:text-gray-900" href="{{route('login')}}">Login</a>
+          {{-- Con el "route" traemos el nombre de la ruta nombrada --}}
+          <a class="font-bold uppercase text-gray-500 text-lg hover:text-gray-900" href="{{route('signup')}}">SignUp</a>
+        </nav>
+      @endguest
     </div>
   </header>
   <main class="container mx-auto mt-10">
