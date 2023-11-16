52096df8da3e129e97b6f52a99ecd7e5f76ce781
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-11-16 00:43:59 -0600

    Almacenando las publicaciones en la BD

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  protected $fillable = [
+    'titulo',
+    'descripcion',
+    'imagen',
+    'user_id'
+  ];
 }
