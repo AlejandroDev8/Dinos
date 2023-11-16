ba20a0aaff8263eb9efa271c00397dd69611e836
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-11-15 23:20:57 -0600

    Usando el factory de Post para testing de la BD

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -9,15 +9,18 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class PostFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'titulo' => $this->faker->sentence(5),
+      'descripcion' => $this->faker->sentence(20),
+      'imagen' => $this->faker->uuid() . '.jpg',
+      'user_id' => $this->faker->randomElement([15, 16, 17])
+    ];
+  }
 }
