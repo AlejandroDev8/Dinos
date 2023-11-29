784db6e1dfeb9f7ae12476f6359e7ea652919c50
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-11-29 04:20:01 -0600

    Obteniendo los usuarios que seguimos (id)

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+  public function __invoke()
+  {
+
+    // Obtener a quienes seguimos
+    dd(auth()->user()->followings->pluck('id')->toArray());
+
+    return view('home');
+  }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 <body class="bg-gray-100">
   <header class="p-5 border-b bg-white shadow">
     <div class="container mx-auto flex justify-between items-center">
-      <a href="/" class="text-3xl font-black uppercase">dinos</a>
+      <a href="{{route('home')}}" class="text-3xl font-black uppercase">dinos</a>
       {{-- Para saber que un usuario está autenticado --}}
       @auth
       <nav class="flex gap-3 items-center">
