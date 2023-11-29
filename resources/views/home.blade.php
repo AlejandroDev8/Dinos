2c196b384c219ef4f1cf59fd38814b1e4952daeb
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2023-11-29 04:36:02 -0600

    Mostrando las publicaciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('title')
-Home
+  Página Inicial
+@endsection
+
+@section('content')
+  @if ($posts->count())
+  <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-3">
+    @foreach ($posts as $post )
+      <div>
+        <a href="{{route('posts.show', ['user' => $post->user, 'post' => $post])}}">
+          <img src="{{asset('uploads') . '/' . $post->imagen}}" alt="Imagen del post {{$post->titulo}}">
+        </a>
+      </div>
+    @endforeach
+  </div>
+  <div class="my-10 p-3">
+    {{$posts->links()}}
+  </div>
+  @else
+    <p class="text-center">No hay publicaciones, sigue a alguien</p>
+  @endif
 @endsection
